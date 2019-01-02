8961eb1330a9222e4388c93f5ff90b5c18d809b2
Neto Deolino <netodeolino@gmail.com>
2019-01-02 19:32:41 -0300

    chamada teste token

--- src/router.js
+++ src/router.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import axios from 'axios'
 
 import Nav from './components/layout/Nav'
 import Footer from './components/layout/Footer'
 import Home from './components/Home'
 import Usuario from './components/Usuario'
 import Login from './components/Login'
+
+import { URL_API, URL_USUARIO, URL_TOKEN_VALIDO } from './util/constants'
 
 Vue.use(Router)
 
@@ -31,8 +34,23 @@ export default new Router({
       },
       beforeEnter: (to, from, next) => {
         if (window.localStorage.getItem('token') != null) {
-          
+          axios
+            .get(`${URL_API}${URL_USUARIO}${URL_TOKEN_VALIDO}`, {
+              headers: {
+                'aqui-nutricionista-token': window.localStorage.getItem('token')
+              }
+            })
+            .then(res => {
+              console.log('------------- ', res.data, ' -------------')
+              next()
+            })
+            .catch(err => {
+              console.log('------------- ', err.response, ' -------------')
+              window.localStorage.removeItem('token')
+              next('/')
+            })
         } else {
+          console.log('------------- ', 'AQUI PORQUE?', ' -------------')
           next('/login')
         }
       }
--- src/util/constants.js
+++ src/util/constants.js
@@ -1,3 +1,4 @@
 export const URL_API = 'http://localhost:4000/'
 export const URL_USUARIO = 'usuario/'
 export const URL_LOGIN = 'login/'
+export const URL_TOKEN_VALIDO = 'token-valido/'
